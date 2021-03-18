db9898abff03ed40c8c5a76ea56497134b1a0ae7
Murugesan, Avudaithangam <Avudaithangam.Murugesan@finastra.com>
2021-03-19 01:00:13 +0530

    first commit

---
+++ src/test/java/com/thangam/initiator/InitiatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangam.initiator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitiatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
